5800cc7aa45e3e43ac025f754d441c9004ed4f40
Vishal <vishal.tyagi19062001@gmail.com>
2025-09-15 09:31:59 +0530

    footer

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,13 +1,19 @@
 
 import React from 'react'
-
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaFacebook } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
+import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
     return (
         <div className='w-[100%]  h-52 mt-[800px] bg-blue-950  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-            
-            <i className="fa-brands fa-instagram"></i>
-            <i className="fa-brands fa-whatsapp"></i>
+            <div className=' flex  gap-4 ml-12'>
+                <div className=' text-5xl  mt-10'><IoLogoWhatsapp /></div>
+                <div className=' text-5xl  mt-10'><FaFacebook /></div>
+                <div className=' text-5xl  mt-10'><CiLinkedin /></div>
+                <div className=' text-5xl  mt-10'><MdEmail /></div>
+            </div>
         </div>
     )
 }
